4eb3b56a446289d6d1a03c8e777f039e5b1f76bb
peterjbone <peterjbone41@gmail.com>
2024-11-03 21:41:35 -0500

    changing the database to mongodb

--- server/src/controllers/login.js
+++ server/src/controllers/login.js
@@ -4,18 +4,30 @@
 const { User } = require("../mongodb.js");
 
 async function login(req, res) {
+	const { email, password } = req.query;
+
 	try {
-		const { email, password } = req.query;
-		if (email && password) {
-			//* 1 validacion: que usuario haya dado email y password
+		/* if (email && password) {
 			const currentUser = await User.findOne({
 				where: { email }
 			});
 
 			if (currentUser) {
-				//* 2 validación: Sí el email esta registrado
 				if (currentUser.password === password) {
-					//* 3 validación: Si hay email con esa password
+					return res.status(200).json({ access: true });
+				}
+				return res.json({ access: false, detail: "password" });
+			}
+			return res.json({ access: false, detail: "email" });
+		} */
+
+		// revisa si llego correo y contraseña
+		if (email && password) {
+			// si encuentra el email
+			const user = await User.findOne({ email: email });
+			if (user) {
+				// si la contraseña coincide
+				if (user.password === password) {
 					return res.status(200).json({ access: true });
 				}
 				return res.json({ access: false, detail: "password" });
@@ -24,7 +36,7 @@ async function login(req, res) {
 		}
 		return res.status(400).send("Faltan datos");
 	} catch (error) {
-		//* Algún error interno de sequelize
+		// Algún error de mongoose o mongodb
 		res.status(500).send(error.message);
 	}
 }
--- server/src/controllers/postUser.js
+++ server/src/controllers/postUser.js
@@ -4,15 +4,30 @@
 const { User } = require("../mongodb.js");
 
 async function postUser(req, res) {
+	const { email, password } = req.body;
+
 	try {
-		const { email, password } = req.body;
-		if (email && password) {
+		/* 		if (email && password) {
 			const [newUser, created] = await User.findOrCreate({
 				where: { email, password }
 			});
 			return created
 				? res.status(200).send("El usuario fue creado")
 				: res.status(200).send("El usuario ya existe.");
+    } */
+
+		if (email && password) {
+			const user = await User.findOne({ email: email, password: password });
+
+			// si no encontro el usuario, lo crea
+			if (!user) {
+				const newUser = new User({ email: email, password: password });
+				await newUser.save();
+				return res.status(200).send("El usuario fue creado.");
+			}
+
+			// si sí encontro al usuario
+			return res.status(200).send("El usuario ya existe.");
 		}
 		return res.status(400).send("Faltan datos");
 	} catch (error) {
