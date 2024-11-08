ed30abb46a17e4c6171f10f97324db78f9737bf4
peterjbone <peterjbone41@gmail.com>
2024-11-08 00:14:35 -0500

    Ignoring the .env files

--- server/src/index.js
+++ server/src/index.js
@@ -1,18 +1,13 @@
 const server = require("./app.js");
 const PORT = process.env.PORT || 3001;
 const { connectDB } = require("./mongodb.js");
-//const { conn } = require("./DB_connection.js");
-
-//* Sincronizar la instancia de sequelize al servidor
-/* conn
-	.sync({ force: true })
-	.then(() => {
-		server.listen(PORT, () => console.log(`Server raised on port ${PORT}`))
-	})
-	.catch((error) => console.error(error.message)) */
 
 //* Levantar el servidor y conectarlo a la base de datos
 server.listen(PORT, async () => {
-	console.log(`Server raised on port ${PORT}`);
-	await connectDB();
+	try {
+		console.log(`Server raised on port ${PORT}`);
+		await connectDB();
+	} catch (error) {
+		console.log(error);
+	}
 });
--- server/src/mongodb.js
+++ server/src/mongodb.js
@@ -2,13 +2,13 @@
 require("dotenv").config();
 const { connect, Schema, model, models } = require("mongoose");
 
-if (!process.env.MONGODB_URL_LOCAL) {
+if (!process.env.MONGODB_URL) {
 	throw new Error("MONGODB_URL must be define.");
 }
 
 async function connectDB() {
 	try {
-		await connect(process.env.MONGODB_URL_LOCAL);
+		await connect(process.env.MONGODB_URL);
 		console.log("Connection established with the database!");
 	} catch (error) {
 		console.log("Error connecting to database", error);
